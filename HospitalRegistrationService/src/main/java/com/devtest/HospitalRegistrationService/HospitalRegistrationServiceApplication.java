b18514f77107c4b761463a9213719c52a52128ef
Carlos Gamboa Encinas <carlitosgamboa17@gmail.com>
2021-02-24 18:43:38 -0400

    dev: Add generated project from Spring Initializr for the backend service.

---
+++ HospitalRegistrationService/src/main/java/com/devtest/HospitalRegistrationService/HospitalRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devtest.HospitalRegistrationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ HospitalRegistrationService/src/test/java/com/devtest/HospitalRegistrationService/HospitalRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtest.HospitalRegistrationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
