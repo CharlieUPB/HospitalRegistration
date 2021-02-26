a803d20582b3a612d48bd247e0723c886f355fd5
Carlos Gamboa Encinas <carlitosgamboa17@gmail.com>
2021-02-25 20:21:08 -0400

    F-001: Add domain entities.

--- HospitalRegistrationService/src/main/java/Audit/AuditableEntity.java
+++ HospitalRegistrationService/src/main/java/Audit/AuditableEntity.java
@@ -17,7 +17,7 @@ public class AuditableEntity {
 	
 	@CreatedBy
 	private String createdBy;
-	
+
 	@CreatedDate
 	private Date createdDate;
 	
@@ -26,5 +26,37 @@ public class AuditableEntity {
 	
 	@LastModifiedDate
 	private Date updatedDate;
+	
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
 
 }
