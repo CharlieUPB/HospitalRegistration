dadf32e7e72b06a4b9d70d963e348dfa8e970fc4
Carlos Gamboa Encinas <carlitosgamboa17@gmail.com>
2021-02-26 01:46:21 -0400

    F-002: Add missing annotation to auditor class.

--- HospitalRegistrationService/src/main/java/com/devtest/HospitalRegistrationService/Audit/AuditAwareImpl.java
+++ HospitalRegistrationService/src/main/java/com/devtest/HospitalRegistrationService/Audit/AuditAwareImpl.java
@@ -3,7 +3,9 @@ package com.devtest.HospitalRegistrationService.Audit;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuditAwareImpl implements AuditorAware<String> {
 	
 	@Override
